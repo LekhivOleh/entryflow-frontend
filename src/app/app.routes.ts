fede23ff386944fe31cffe7f1dff5897e3d97479
LekhivOleh <oleh.lekhiv.ir.2023@lpnu.ua>
2025-03-30 13:17:23 +0300

    add project files

---
+++ src/app/app.routes.ts
@@ -0,0 +1,35 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => {
+      return import('./home/home.component').then(m => m.HomeComponent);
+    },
+  },
+  {
+    path: 'register',
+    loadComponent: () => {
+      return import('./register/register.component').then(m => m.RegisterComponent);
+    }
+  },
+  {
+    path: 'login',
+    loadComponent: () => {
+      return import('./login/login.component').then(m => m.LoginComponent);
+    }
+  },
+  {
+    path: 'action-log',
+    loadComponent: () => {
+      return import('./action-log/action-log.component').then(m => m.ActionLogComponent);
+    }
+  },
+  {
+    path: 'employee/:employeeId',
+    loadComponent: () => {
+      return import('./employee-page/employee-page.component').then(m => m.EmployeePageComponent);
+    }
+  }
+];
