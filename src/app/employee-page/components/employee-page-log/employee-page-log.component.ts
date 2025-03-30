1254730b0bb54402e99e3e9f85e1fbbfc763ffd6
LekhivOleh <oleh.lekhiv.ir.2023@lpnu.ua>
2025-03-30 16:05:14 +0300

    add employee-page log and graph placeholder

--- src/app/employee-page/components/employee-page-log/employee-page-log.component.ts
+++ src/app/employee-page/components/employee-page-log/employee-page-log.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import employeesList from '../../../shared/utils/employees';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-employee-page-log',
-  imports: [],
+  standalone: true,
+  imports: [
+    NgIf
+  ],
   templateUrl: './employee-page-log.component.html',
   styleUrl: './employee-page-log.component.css'
 })
 export class EmployeePageLogComponent {
+  employee: any;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      const id = params.get('id');
+      this.employee = employeesList.find(emp => emp.id === Number(id));
+    });
+  }
 }
